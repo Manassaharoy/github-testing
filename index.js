496911a1334eae388a5814cebd447aaab3d7de27
Manassaharoy <triptoroy5@gmail.com>
2023-05-23 12:34:13 +0600

    frontend added

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
+
 // Added this line in main
 app.get("/dev", (req, res) => {
   res.send("Hello wold!");
