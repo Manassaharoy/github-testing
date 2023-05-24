b214312ff5d2927712dd486edce197e3b3fb90ff
Manassaharoy <triptoroy5@gmail.com>
2023-05-24 16:03:20 +0600

    up

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const express = require("express");
 const cors = require("cors");
 
-const path = require("path")
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, '/frontend/build')))
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ extended: true }));
 
 // Added this line in main
 app.get("/dev", (req, res) => {
   res.send("Hello wold!");
 });
 
+app.post("/test", (req, res) => {
+  console.log(req.body.data);
+  res.send(req.body.data);
+});
 //Hello FROM DEVELOPMENT
 
 // GENERATING MERGE FAIL
